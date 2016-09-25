11f18e10567cc8c35d50f47040c41fa60a4ebf2b
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2016-09-25 21:07:22 +0200

    footer
    footer

--- app/components/main-box.js
+++ app/components/main-box.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['main-box']
+  classNames: ['main-box'],
+
+  thisYear: function() {
+    let d = new Date();
+    return d.getFullYear();
+  }.property()
+
 });
--- app/components/social-icons.js
+++ app/components/social-icons.js
@@ -4,8 +4,8 @@ export default Ember.Component.extend({
   classNames: ['social-icons'],
   routing: Ember.inject.service('-routing'),
 
-  notIndex: Ember.computed(function() {
+  notIndex: function() {
     return this.get('routing.currentRouteName')!=='index';
-  })
+  }.property()
 
 });
