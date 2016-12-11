354d64e3fcc3f21b8f49c02c5b85131f068d7f92
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2016-12-11 08:10:15 +0100

    one more video for page learn
    one more video for page learn

--- app/routes/learn.js
+++ app/routes/learn.js
@@ -13,30 +13,40 @@ export default Ember.Route.extend({
     */
 
     this.store.push({
-      data: [{
-        id: 1,
-        type: 'video',
-        attributes: {
-          title: "Competitive Liquidity Options",
-          url: "https://www.youtube.com/watch?v=NJm7Msu0Nmk"
+      data: [
+        {
+          id: 1,
+          type: 'video',
+          attributes: {
+            title: "How Ripple Works",
+            url: "https://www.youtube.com/watch?v=5e2jDFL1kKI"
+          }
+        },
+        {
+          id: 2,
+          type: 'video',
+          attributes: {
+            title: "Competitive Liquidity Options",
+            url: "https://www.youtube.com/watch?v=NJm7Msu0Nmk"
+          }
+        },
+        {
+          id: 3,
+          type: 'video',
+          attributes: {
+            title: "Retail Remittances",
+            url: "https://www.youtube.com/watch?v=ZckgnFDUOU8"
+          }
+        },
+        {
+          id: 4,
+          type: 'video',
+          attributes: {
+            title: "Corporate Disbursements",
+            url: "https://www.youtube.com/watch?v=BWgCQVEr-rc"
+          }
         }
-      },
-      {
-        id: 2,
-        type: 'video',
-        attributes: {
-          title: "Retail Remittances",
-          url: "https://www.youtube.com/watch?v=ZckgnFDUOU8"
-        }
-      },
-      {
-        id: 3,
-        type: 'video',
-        attributes: {
-          title: "Corporate Disbursements",
-          url: "https://www.youtube.com/watch?v=BWgCQVEr-rc"
-        }
-      } ]
+      ]
     });
 
     return this.store.peekAll('video');
