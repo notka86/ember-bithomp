986798fb7062843ecb38d695186d545570b9c689
Viacheslav Bakshaev <bakshaev@protonmail.ch>
2017-04-15 07:13:48 +0200

    cleaning up a bit
    cleaning up a bit

--- app/controllers/create.js
+++ app/controllers/create.js
@@ -18,19 +18,15 @@ export default Ember.Controller.extend({
   /* 
     generate: function() {
       count = count + 1;
-      var api = new ripple.RippleAPI({});
+      var api = new ripple.RippleAPI();
 
       var account = api.generateAddress();
       console.log(count);
       var string = account.address;
       var result = string.match(/ripple/i);
-      var result2 = string.match(/ihomp/i);
-      var result3 = string.match(/slava/i);
-      var result4 = string.match(/safello/i);
-      var result5 = string.match(/bithomp/i);
-      var result6 = string.match(/russian/i);
+      var result2 = string.match(/bithomp/i);
 
-      if (result || result2 || result3 || result4 || result5 || result6) {
+      if (result || result2) {
         console.log(account.address);
         console.log(account.secret);
       } else {
